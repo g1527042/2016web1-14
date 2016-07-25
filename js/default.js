66516204170bf3e3dc5095e41afe293538b078e3
g1527042 <g1527042@st.t-kougei.ac.jp>
2016-07-25 17:02:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,10 @@
 // 確認ダイアログを表示
 var result = window.confirm('準備OK？');
 // 結果を表示
-document.getElementById('choice').textContent = result;
+if(result){
+  document.getElementById('choice').textContent = 'OKが押されました';
+}else{
+  document.getElementById('choice').textContent = 'キャンセルが押されました';
+}
+
 
